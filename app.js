e32d5ca2d2b09f16a021b728302fd1f1344d9fb2
firdavsxon@gmail.com <firdavsxon@gmail.com>
2024-08-15 01:10:48 +0500

    2-vazifa 5- oy

---
+++ app.js
@@ -0,0 +1,44 @@
+// function User (name, age,phone, email) {
+//     this.name = name;
+//     this.age  = age ;
+//     this.phone = phone ;
+//     this.email =email;
+// }
+// let John = new User("John ,"45."+14688", "john'gmail.com") ;
+// console.log(john) 
+
+// let user = {
+//    name: "john",
+//    age: 45,
+// //    address: {
+// //        city: "Tashkent",
+       
+// //    }
+// }
+
+// console.log(user .address?.city);
+
+
+
+// Uyga vazifa masallar
+
+ // 1- masalla
+
+// function sortArrayByLengthDescending(array) {
+//   return array.sort((a, b) => b.length - a.length);
+// }
+// const myArray = ["inson", "daraxtlar", "o'lim", "avtomabil", "olmalar"];
+// const sortedArray = sortArrayByLengthDescending(myArray);
+
+// console.log(sortedArray);
+
+
+//  2- masala
+
+//  function capitalizeFirstLetters(array) {
+//   return array.map(el => typeof el === 'string' ? el[0].toUpperCase() + el.slice(1) : el);
+// }
+
+// const myArray = ["salom",  18, "Najot", "dost"];
+// const capitalizedArray = capitalizeFirstLetters(myArray);
+// console.log(capitalizedArray); 
